579fbedca62fbfccc6cf1519d3433772e52313af
Aleksey Krupsky <dachadybenci2@gmail.com>
2022-06-27 16:43:56 +0300

    WEEK 5. Set up types

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ const typeDefs = gql`
   # The "Query" type is special: it lists all of the available queries that
   # clients can execute, along with the return type for each. In this
   # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-  }
+#  type Query {
+#    books: [Book]
+#  }
 `;
 
 const books = [
